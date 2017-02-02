ee03c1b942e5bfb9824498bcd40c5d55f12e53f7
Rambunction Crew <rambunction4330@gmail.com>
2017-02-02 17:52:01 -0600

    y

--- Screambunction/src/frc/team4330/screambunction/subsystems/AutonomousManager.java
+++ Screambunction/src/frc/team4330/screambunction/subsystems/AutonomousManager.java
@@ -19,8 +19,7 @@ public class AutonomousManager extends Subsystem {
 		CommandGroup group = new CommandGroup();
 //		group.addSequential(new DriveForward(RobotMap.DISTANCE_TO_BASELINES
 //				+ RobotMap.ROBOT_WIDTH));
-		group.addSequential(new DriveForward(10));
-//		group.addSequential(new Turn(-90));
+		group.addSequential(new Turn(-90));
 		
 		Scheduler.getInstance().add(group);
 	}
